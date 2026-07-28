817f22e54bef8c20556f05b8f9f15d89cab25a4a
HoangChuong <hosonhc@gmail.com>
2026-07-29 02:23:02 +0700

    update

--- happy birtday.js
+++ happy birtday.js
@@ -1,7 +1,7 @@
 let w = (c.width = window.innerWidth),
   h = (c.height = window.innerHeight),
   ctx = c.getContext("2d"),
-  hw = w / 2 - 30;
+  hw = w / 2 - 40;
 (hh = h / 2),
   (opts = {
     // change the text in here //
