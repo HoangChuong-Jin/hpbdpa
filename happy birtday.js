81341f8a975b4d153a5cbe82e8636b1eda57ed51
HoangChuong-Jin <82763012+HoangChuong-Jin@users.noreply.github.com>
2026-07-29 04:49:48 +0700

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -1,12 +1,12 @@
 let w = (c.width = window.innerWidth),
   h = (c.height = window.innerHeight),
   ctx = c.getContext("2d"),
-  hw = w / 2 - 5;
+  hw = w / 2;
 (hh = h / 2),
   (opts = {
     // change the text in here //
     strings: ["HAPPY", "BIRTHDAY!", "PHƯƠNG ANH"],
-    charSize: 28,
+    charSize: 24,
     charSpacing: 35,
     lineHeight: 40,
 
